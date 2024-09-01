42c87c566f57389a8207db1f89e710ddf6e18e37
Milos Stefanov <144391186+MilosBC@users.noreply.github.com>
2024-09-01 02:12:32 +0200

    Write the player switch logic
    The logic for switching the players now works as intended

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let activeGame = true;
+
+
 function player() {
   let name;
   let marker;
@@ -6,18 +9,21 @@ function player() {
   const setName = () => name = prompt('Please enter your name');
   
 
-  const getName = () =>  console.log(`Player ${marker === 'X' ? 1 : 2} name: ${name}. Marker: ${marker}`);
+  const playerDetails = () =>  console.log(`Player ${marker === 'X' ? 1 : 2} name: ${name}. Marker: ${marker}`);
+
+  const getName = ()=> name;
     
   
 
   const setMarker = (markerPlaceholder) => marker = markerPlaceholder;
   const putMarker = () => marker; 
 
-  return {setName, getName, setMarker, putMarker};
+  return {setName, getName, playerDetails, setMarker, putMarker};
 } 
 
 const gameboard = (function() {
     let boardArray = [1,2,3,4,5,6,7,8,9];
+    let markers = [];
     const displayBoard = () => console.log(`
     ${boardArray[0]}   ${boardArray[1]}   ${boardArray[2]} 
     
@@ -25,24 +31,69 @@ const gameboard = (function() {
     
     ${boardArray[6]}   ${boardArray[7]}   ${boardArray[8]}`);
 
-    return {displayBoard};
+    const pushMarker = marker => markers.push(marker);
+
+    const choosePosition = numberInput => {
+     boardArray.splice(numberInput, 1, `${markers[markers.length - 1] === 'X' ? 'X' : 'O'}`);
+     
+      
+    }
+
+    
+
+    return {displayBoard, pushMarker, choosePosition};
 }) ();
+
+
 
 console.log('Welcome to Tic Tac Toe! Good luck everyone and may the better player win!');
 
 const playerOne = player();
 playerOne.setName();
 playerOne.setMarker('X');
-playerOne.getName();
+playerOne.playerDetails();
 
 
 const playerTwo = player();
 playerTwo.setName();
 playerTwo.setMarker('O');
-playerTwo.getName();
+playerTwo.playerDetails();
 
 
 gameboard.displayBoard();
+
+
+function gameFlow() {
+  
+  let activePlayer = 1;
+
+  const switchActivePlayer = () => {
+    if (activePlayer === 1) {
+     const playerOneInput = Number(prompt(`${playerOne.getName()}, where do you want to place your mark! Choose the number between 1 and 9`));
+     gameboard.pushMarker(playerOne.putMarker());
+     gameboard.choosePosition(playerOneInput);
+     gameboard.displayBoard();
+     activePlayer = 2;
+     
+
+    } else if (activePlayer === 2) {
+      const playerTwoInput = Number(prompt(`${playerTwo.getName()}, where do you want to place your mark! Choose the number between 1 and 9`));
+     gameboard.pushMarker(playerTwo.putMarker());
+     gameboard.choosePosition(playerTwoInput);
+     gameboard.displayBoard();
+     activePlayer = 1;
+    }
+  }
+
+  return {switchActivePlayer};
+}
+
+const newGame = gameFlow();
+
+newGame.switchActivePlayer(); 
+newGame.switchActivePlayer(); 
+newGame.switchActivePlayer(); 
+newGame.switchActivePlayer(); 
 
 
 
